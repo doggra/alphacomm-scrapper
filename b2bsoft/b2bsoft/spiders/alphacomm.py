bba3dd88214c9906fe2d9a8b593410859414d2b0
Doggra <doggra@protonmail.com>
2017-02-28 20:22:32 +0100

    working prototype

--- b2bsoft/b2bsoft/spiders/alphacomm.py
+++ b2bsoft/b2bsoft/spiders/alphacomm.py
@@ -1,26 +1,75 @@
 # -*- coding: utf-8 -*-
+
+__author__ = "Radosław (Doggra) Przytuła"
+__email__ = "doggra@protonmail.com"
+
 import scrapy
 import json
+from scrapy.exceptions import CloseSpider
+from b2bsoft.items import ShopItem
+
+def get_json_from_response(response):
+    """ Get json from API response """
+    return json.loads(response.body_as_unicode())
+
+def close_spider(message=""):
+    """ Close spider with msg to logger """
+    raise CloseSpider(reason=message)
 
 
 class AlphacommSpider(scrapy.Spider):
+    """ Shopalphacomm.com spider """
+
     name = "alphacomm"
     allowed_domains = ["shopalphacomm.com"]
-    start_urls = ['http://shopalphacomm.com/api/items?include=facets&fieldset=detail']
+    start_urls = ['http://shopalphacomm.com/api/items?include=facets'\
+                                                  +'&fieldset=detail']
+
+    def __init__(self):
+        self.scrapped_sku = []
 
     def parse(self, response):
-        """ Get all item groups """
+        """ Initial crawling function. Get all item groups 
+        """
 
-        jsonresponse = json.loads(response.body_as_unicode())
+        jsonresponse = get_json_from_response(response)
 
-        # Fetch "facets" dictionary
-        facets = jsonresponse['facets']
+        # Find dicitionary containing customitem groups and make a list of it
+        try:
+            custitem_groupid = next((d for d in jsonresponse['facets']\
+                              if d['id'] == "custitem_groupid"), None)
+        except KeyError:
+            close_spider('No `facets` dict found')
 
-        # Fetch items groups dictionary
-        custitem_groupids_dict = facets[3]
+        item_groups_ids = [ d['label'] for d in custitem_groupid['values']\
+                                                         if 'label' in d ]
 
-        # Check if fetched correct dictionary and get all groups IDs
-        if custitem_groupids_dict['id'] == "custitem_groupid":
-            item_groups_ids = [ d['label'] for d in custitem_groupids_dict['values'] if 'label' in d ]
+        # Generate requests for item groups
+        for group_id in item_groups_ids[:2]:
+            yield scrapy.Request(
 
-        print(item_groups_ids)
+                "http://shopalphacomm.com/api/items?include=facets&"\
+                    +"custitem_groupid={}&fieldset=details".format(group_id,),
+
+                callback=self.item_group_details,
+                meta={'groupid': group_id}
+            )
+
+    def item_group_details(self, response):
+        """ Get specific item group details 
+        """
+
+        item = ShopItem()
+        jsonresponse = get_json_from_response(response)
+
+        # Get list of group items
+        items_list = jsonresponse['items']
+
+        brand_dict = next((d for d in jsonresponse['facets'] \
+                  if d['id'] == "custitem_sca_brand"), None)
+
+        brands_list = ' '.join([ b['label'] for b in brand_dict['values'] ])
+
+        item['manufacturer'] = brands_list
+
+        yield item
