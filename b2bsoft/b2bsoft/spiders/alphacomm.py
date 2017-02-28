a5761cf44a356342a19054101519663164f7a40f
Doggra <doggra@protonmail.com>
2017-02-28 16:40:59 +0100

    developing spider: get all items groups

--- b2bsoft/b2bsoft/spiders/alphacomm.py
+++ b2bsoft/b2bsoft/spiders/alphacomm.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 
 
 class AlphacommSpider(scrapy.Spider):
     name = "alphacomm"
     allowed_domains = ["shopalphacomm.com"]
-    start_urls = ['http://shopalphacomm.com/']
+    start_urls = ['http://shopalphacomm.com/api/items?include=facets&fieldset=detail']
 
     def parse(self, response):
-        pass
+        """ Get all item groups """
+
+        jsonresponse = json.loads(response.body_as_unicode())
+
+        # Fetch "facets" dictionary
+        facets = jsonresponse['facets']
+
+        # Fetch items groups dictionary
+        custitem_groupids_dict = facets[3]
+
+        # Check if fetched correct dictionary and get all groups IDs
+        if custitem_groupids_dict['id'] == "custitem_groupid":
+            item_groups_ids = [ d['label'] for d in custitem_groupids_dict['values'] if 'label' in d ]
+
+        print(item_groups_ids)
---
+++ b2bsoft/docs/urls.rst
@@ -0,0 +1,2 @@
+# URL to all items groups
+http://shopalphacomm.com/api/items?include=facets&fieldset=detail&limit=100
