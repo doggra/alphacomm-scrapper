04ff1ec209c75910d387e8d035263fc19976787e
Doggra <doggra@protonmail.com>
2017-02-28 20:21:01 +0100

    set up settings

--- b2bsoft/b2bsoft/settings.py
+++ b2bsoft/b2bsoft/settings.py
@@ -1,72 +1,41 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for b2bsoft project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'b2bsoft'
-
 SPIDER_MODULES = ['b2bsoft.spiders']
 NEWSPIDER_MODULE = 'b2bsoft.spiders'
 
+USER_AGENT = 'b2bsoft (+http://www.yourdomain.com)'
+ROBOTSTXT_OBEY = False
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'b2bsoft (+http://www.yourdomain.com)'
-
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
-
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS = 32
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
-# Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
 
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
 #    'b2bsoft.middlewares.B2BsoftSpiderMiddleware': 543,
 #}
 
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
 #    'b2bsoft.middlewares.MyCustomDownloaderMiddleware': 543,
 #}
 
-# Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
-# Configure item pipelines
-# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
 #    'b2bsoft.pipelines.B2BsoftPipeline': 300,
 #}
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -80,6 +49,7 @@ ROBOTSTXT_OBEY = True
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
+
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
