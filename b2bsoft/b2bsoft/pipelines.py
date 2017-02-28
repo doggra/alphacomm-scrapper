cc339bfd73e704f799bd5fc2a0e5971f5f28703b
Doggra <doggra@protonmail.com>
2017-02-28 23:20:17 +0100

    clean pipelines.py from useless comments

--- b2bsoft/b2bsoft/pipelines.py
+++ b2bsoft/b2bsoft/pipelines.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from scrapy import signals
 
 class B2BsoftPipeline(object):
     def process_item(self, item, spider):
