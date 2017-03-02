1a8cf64a88d747bec3c53016a97cd8aab8d19733
Doggra <doggra@protonmail.com>
2017-03-02 17:16:45 +0100

    fix indent and comment

--- b2bsoft/b2bsoft/utils.py
+++ b2bsoft/b2bsoft/utils.py
@@ -1,19 +1,18 @@
-""" Some helper functions """
+""" Some utils functions """
 
 import json
 from b2bsoft.items import ShopItem
 from HTMLParser import HTMLParser
 
-
 def create_new_item():
-	it = ShopItem()
-	it['vendor'] = "Alphacomm"
-	it['is_serial'] = 0
-	it['cost'] = 0
-	it['msrp'] = 0
-	it['department'] = "Accessory"
-	it['active'] = 1
-	return it
+    it = ShopItem()
+    it['vendor'] = "Alphacomm"
+    it['is_serial'] = 0
+    it['cost'] = 0
+    it['msrp'] = 0
+    it['department'] = "Accessory"
+    it['active'] = 1
+    return it
 
 def get_json_from_response(response):
     """ Get json from API response """
